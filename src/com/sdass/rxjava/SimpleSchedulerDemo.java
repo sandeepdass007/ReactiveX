51c21ace82d77fd9c7546aa82d9408646c2642bc
Sandeep Dass <sandeepdass003@gmail.com>
2019-03-16 20:15:01 +0530

    Error handling

--- src/com/sdass/rxjava/SimpleSchedulerDemo.java
+++ src/com/sdass/rxjava/SimpleSchedulerDemo.java
@@ -9,14 +9,18 @@ public class SimpleSchedulerDemo {
 	}
 	
 	private static void basicSchedulerExample() {
-		Observable.range(1, 10)
-			.map(value -> value * 2)
+		Observable.range(0, 10)
+			.map(value -> value / (value - 1))
+			.onErrorReturn(error -> {
+				System.out.println("Error Occured: " +  error.getMessage());
+				return -1;
+			})
 			.observeOn(Schedulers.computation())
 			.subscribe(System.out::println);
 		try {
+			// Main Thread needs to wait
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
